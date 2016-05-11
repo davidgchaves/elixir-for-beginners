f0bfc5620e3988c690ac74ce012833c0e0a58236
David G Chaves <davidgchaves@gmail.com>
2016-05-11 13:49:43 +0200

    Working ping-pong process game
    ✔ elixir ping_pong.exs
    1st_player: #PID<0.47.0>
    2nd_player: #PID<0.53.0>
    1. #PID<0.47.0> hit_to #PID<0.53.0> :ping
    2. #PID<0.53.0> hit_to #PID<0.47.0> :pong
    3. #PID<0.47.0> hit_to #PID<0.53.0> :ping
    4. #PID<0.53.0> hit_to #PID<0.47.0> :pong
    5. #PID<0.47.0> hit_to #PID<0.53.0> :ping
    6. #PID<0.53.0> hit_to #PID<0.47.0> :pong
    7. #PID<0.47.0> hit_to #PID<0.53.0> :ping
    8. #PID<0.53.0> hit_to #PID<0.47.0> :pong
    9. #PID<0.47.0> hit_to #PID<0.53.0> :ping
    10. #PID<0.53.0> hit_to #PID<0.47.0> :pong
    11. #PID<0.47.0> hit_to #PID<0.53.0> :ping
    And #PID<0.47.0> wins with a final :ping on round {11}
    Timing out player #PID<0.47.0>
    Timing out player #PID<0.53.0>

---
+++ ping_pong.exs
@@ -0,0 +1,40 @@
+defmodule PingPong do
+  def ready do
+    receive do
+      {sender, action, 11} ->
+        IO.puts "And #{inspect sender} wins with a final #{inspect action} on round {11}"
+        ready
+
+      {sender, action, turn} ->
+        hit_to sender, switch(action), turn+1
+        ready
+
+    after
+      1_000 -> IO.puts "Timing out player #{inspect player_pid}"
+    end
+  end
+
+  def hit_to(opponent_id, action, turn) do
+    IO.puts "#{turn}. #{inspect player_pid} hit_to #{inspect opponent_id} #{inspect action}"
+    send opponent_id, {player_pid, action, turn}
+  end
+
+  defp switch(action) do
+    case action do
+      :ping -> :pong
+      _     -> :ping
+    end
+  end
+
+  defp player_pid, do: self
+end
+
+player_1 = self
+player_2 = spawn(PingPong, :ready, [])
+
+IO.puts "1st_player: #{inspect player_1}"
+IO.puts "2nd_player: #{inspect player_2}"
+
+PingPong.hit_to player_2, :ping, 1
+
+PingPong.ready
