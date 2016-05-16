19f4e9095054cb2d194d0f2d59faacefbe72d2fa
David G Chaves <davidgchaves@gmail.com>
2016-05-16 13:45:58 +0200

    Add `ex_doc` and `earmark` dependencies

--- caesar/mix.exs
+++ caesar/mix.exs
@@ -27,6 +27,9 @@ defmodule Caesar.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.11"},
+      {:earmark, "~> 0.2"}
+    ]
   end
 end
