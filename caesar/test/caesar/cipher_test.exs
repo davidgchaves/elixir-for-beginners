04da2a3bcba50849e34df76ebfd5b235073f29da
David G Chaves <davidgchaves@gmail.com>
2016-05-16 14:36:57 +0200

    Add two more tests for edge cases

--- caesar/test/caesar/cipher_test.exs
+++ caesar/test/caesar/cipher_test.exs
@@ -11,4 +11,12 @@ defmodule Caesar.CipherTest do
   test "encrypt handles capital letters" do
     assert encrypt("abCD", 1) == "zaBC"
   end
+
+  test "encrypt handles spaces" do
+    assert encrypt("ab cd", 1) == "za bc"
+  end
+
+  test "encrypt handles shift values greater than 26" do
+    assert encrypt("abcd", 27) == "zabc"
+  end
 end
