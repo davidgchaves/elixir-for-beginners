d846b5ffffa3d3f42a363f4c243d29f488a1485d
David G Chaves <davidgchaves@gmail.com>
2016-05-21 11:12:51 +0200

    Command line tool working

--- caesar/lib/caesar.ex
+++ caesar/lib/caesar.ex
@@ -2,6 +2,7 @@ defmodule Caesar do
   def main(argv) do
     argv
       |> parse_args
+      |> process
     System.halt(0)
   end
 
@@ -12,5 +13,16 @@ defmodule Caesar do
       {[shift: shift], ["encrypt", msg], _} -> {:encrypt, msg, shift |> String.to_integer}
       {_             , _               , _} -> {:help}
     end
+  end
+
+  def process({:encrypt, msg, shift}) do
+    Caesar.Cipher.encrypt(msg, shift)
+      |> IO.puts
+  end
+
+  def process({:help}) do
+    IO.puts """
+    usage: ./caesar <command> <message> [--shift <shift_number>]
+    """
   end
 end
